0024cc9195d12e0095531288d991271627da915a
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2024-09-05 00:03:10 +0900

    add: 修飾子

---
+++ modifier.dart
@@ -0,0 +1,11 @@
+void main() {
+  const int a = 1;
+  const String message = "hello world!!";
+  print(a);
+  print(message);
+  final bool messageIsEmpty = message.isEmpty;
+  print(messageIsEmpty);
+  late int id;
+  id = 1;
+  print(id);
+}
